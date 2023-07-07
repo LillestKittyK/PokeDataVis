feb7b0d9feeb8aad77d7b0fb04b6f83dbce7c173
LillestKittyK <128289642+LillestKittyK@users.noreply.github.com>
2023-07-07 19:43:49 +0100

    added auto complete

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var i;
 var currentlyOpen = null;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     if (currentlyOpen && currentlyOpen !== this) {
       currentlyOpen.classList.remove("active");
       currentlyOpen.nextElementSibling.style.maxHeight = null;
@@ -20,3 +20,48 @@ for (i = 0; i < acc.length; i++) {
     currentlyOpen = this.classList.contains("active") ? this : null;
   });
 }
+
+const startTime = performance.now();
+
+fetch('poke/pokedex.json')
+  .then(response => response.json())
+  .then(data => {
+    const result = data.map(item => [item.id, item.name.english]);
+    console.log(result);
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+    console.log(`Execution time: ${executionTime} milliseconds`);
+
+    document.getElementById('search').addEventListener('keyup', searchPokemon);
+
+    function searchPokemon() {
+      const searchInput = document.getElementById('search').value.toLowerCase();
+      const matchedPokemon = data.filter(item => item.name.english.toLowerCase().includes(searchInput));
+      const roundedBox = document.querySelector('.rounded-box');
+
+      if (matchedPokemon.length > 0) {
+        const listItems = matchedPokemon.map(item => {
+          const pokemonData = result.find(dataItem => dataItem[1] === item.name.english);
+          return `<li><a href="#" data-id="${pokemonData[0]}">${pokemonData[1]}</a></li>`;
+        }).join('');
+
+        const numResults = matchedPokemon.length;
+        roundedBox.innerHTML = `<p>${numResults} result${numResults > 1 ? 's' : ''} found</p><ul>${listItems}</ul>`;
+
+        const links = roundedBox.querySelectorAll('a');
+        links.forEach(link => {
+          link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const pokemonId = Number(this.dataset.id);
+            const pokemon = data.find(item => item.id === pokemonId);
+            console.log(pokemon);
+          });
+        });
+      } else {
+        roundedBox.innerHTML = 'No pokemon found';
+      }
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
